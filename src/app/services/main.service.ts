9e770cf5758e9af0ec2020d0d9f96780517d9e3c
anurdin <adamnurdin01@gmail.com>
2021-10-27 10:51:58 +0700

    Update environment

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class MainService {
 	constructor(private http: HttpClient) { }
 
 	getPosts(){
-		return this.http.get("https://app.villume.com/check-health");
+		return this.http.get(environment.baseUrl + "/check-health");
 	}
 }
